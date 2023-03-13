78a4e80f6b5c1e5790d1ff09f1381632d7f6170c
Robert O'Shea <robertoshea2k11@gmail.com>
2023-03-13 20:48:11 +0000

    Fixed stupid mistake

---
+++ pitd/scene/__init__.py
@@ -0,0 +1,23 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any, Self
+import pygame
+
+__all__ = ["Scene"]
+
+
+class Scene(metaclass=ABCMeta):
+    @abstractmethod
+    def render(self, surface: pygame.Surface) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, dt: float) -> Self:
+        pass
+
+    @abstractmethod
+    def get_event(self, event: pygame.Event) -> None:
+        pass
+
+    @abstractmethod
+    def set_data(self, name: str, val: Any) -> Any:
+        pass
