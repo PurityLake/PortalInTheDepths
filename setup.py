739113e4a6bce0d562c8a01906875d8eb45a0691
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-07 23:23:51 +0000

    Fixed issue with file checks

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import PyInstaller.__main__
 
 
 def check_types() -> None:
-    print(f"> mypy pitd.py pitd/")
-    results = api.run(["pitd.py", "pitd/"])
+    print("> mypy pitd.py pitd/")
+    results = api.run(["game.py", "pitd/"])
     if results[0]:
         print("Type checking results:")
         print(results[0])
