2b9deb1a16bfb6044add1aa7753edfd2e84bb97e
Robert O'Shea <robertoshea2k11@gmail.com>
2024-01-08 22:32:09 +0000

    Added a bsp scene

---
+++ pitd/scene/bsp_scene.py
@@ -0,0 +1,30 @@
+from . import Scene
+from ..map import bsp
+
+import pygame
+from typing import Self, Any
+
+
+class BSPScene(Scene):
+    def __init__(self):
+        super().__init__()
+        self._should_quit = False
+        self.bsp = bsp.BSP(600, 600, 80, 200)
+        self.bsp.generate(None, 5)
+
+    def render(self, surface: pygame.Surface) -> None:
+        surface.fill((0, 0, 0))
+        self.bsp.debug_render(surface)
+        pygame.display.flip()
+
+    def update(self, dt: float) -> Self:
+        return self
+
+    def get_event(self, event: pygame.Event) -> None:
+        pass
+
+    def set_data(self, name: str, value: Any) -> None:
+        pass
+
+    def should_quit(self) -> bool:
+        return self._should_quit
